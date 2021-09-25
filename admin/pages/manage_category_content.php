57ee760ce907437694ab2f447f33ea322675bb0c
Ahmed Shahriar Kabir <84874409+shrkabir@users.noreply.github.com>
2021-09-26 00:35:22 +0600

    Update manage_category_content.php

--- admin/pages/manage_category_content.php
+++ admin/pages/manage_category_content.php
@@ -25,7 +25,7 @@
 <div class="row-fluid sortable">		
 				<div class="box span12">
 					<div class="box-header" data-original-title>
-						<h2><i class="halflings-icon user"></i><span class="break"></span>Members</h2>
+						<h3><i class="halflings-icon user"></i><span class="break"></span>Members</h3>
 						<div class="box-icon">
 							<a href="#" class="btn-setting"><i class="halflings-icon wrench"></i></a>
 							<a href="#" class="btn-minimize"><i class="halflings-icon chevron-up"></i></a>
