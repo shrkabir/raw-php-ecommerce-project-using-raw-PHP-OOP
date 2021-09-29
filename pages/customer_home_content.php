2deadcb1e86e1509b57a747ddcba0075c4c13295
Ahmed Shahriar Kabir <84874409+shrkabir@users.noreply.github.com>
2021-09-29 19:26:01 +0600

    Update customer_home_content.php file.

--- pages/customer_home_content.php
+++ pages/customer_home_content.php
@@ -2,7 +2,7 @@
 	<div class="row">
 		<div class="col-md-12">
 			<div class="well">
-				<h3>Thanks <?php echo $_SESSION['customer_name']; ?> for you valuable order. We will contact with you very soon...</h3>
+				<h2>Thanks <?php echo $_SESSION['customer_name']; ?> for you valuable order. We will contact with you very soon...</h2>
 			</div>
 		</div>
 	</div>
